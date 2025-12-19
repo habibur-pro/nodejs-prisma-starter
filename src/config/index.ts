25aa0ed99287cae12f03c40ede4a6f9b08ad4d06
Habibur Rahman <va.habibur@gmail.com>
2025-12-19 12:47:58 +0600

    fix: fix uploader isuee

--- src/config/index.ts
+++ src/config/index.ts
@@ -40,10 +40,17 @@ export default {
     apiSecret: process.env.CLOUDINARY_API_SECRET,
   },
 
+  // s3: {
+  //   aws_s3_region: process.env.AWS_S3_REGION,
+  //   aws_s3_access_key: process.env.AWS_S3_ACCESS_KEY,
+  //   aws_s3_secret_key: process.env.AWS_S3_SECRET_KEY,
+  //   aws_s3_bucket: process.env.AWS_S3_BUCKET,
+  // },
+
   s3: {
-    aws_s3_region: process.env.AWS_S3_REGION,
-    aws_s3_access_key: process.env.AWS_S3_ACCESS_KEY,
-    aws_s3_secret_key: process.env.AWS_S3_SECRET_KEY,
-    aws_s3_bucket: process.env.AWS_S3_BUCKET,
+    do_space_endpoint: process.env.DO_SPACE_ENDPOINT,
+    do_space_accesskey: process.env.DO_SPACE_ACCESS_KEY,
+    do_space_secret_key: process.env.DO_SPACE_SECRET_KEY,
+    do_space_bucket: process.env.DO_SPACE_BUCKET,
   },
 };
